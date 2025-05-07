6d65a28ec1b8d131a50fd0d5753cef3a62e8a859
Dzmitry_Poliukou <dzmitry_poliukou@epam.com>
2025-05-07 16:45:16 +0400

    Initial commit

---
+++ java-advanced-training/src/main/java/com/epam/java_advanced_training/JavaAdvancedTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.epam.java_advanced_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdvancedTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAdvancedTrainingApplication.class, args);
+	}
+
+}
---
+++ java-advanced-training/src/test/java/com/epam/java_advanced_training/JavaAdvancedTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.java_advanced_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAdvancedTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
