de09695d07462a4d59fa4f32d2b9e56d80d3fda4
Dzmitry_Poliukou <dzmitry_poliukou@epam.com>
2025-05-08 11:53:03 +0400

    controller advise responseBody

--- java-advanced-training/src/main/java/com/epam/java_advanced_training/handler/GlobalExceptionHandler.java
+++ java-advanced-training/src/main/java/com/epam/java_advanced_training/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.epam.java_advanced_training.exception.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
@@ -11,12 +12,14 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(ProductNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ResponseBody
   public String handleProductNotFoundException(ProductNotFoundException ex) {
     return ex.getMessage();
   }
 
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ResponseBody
   public String handleGenericException(Exception ex) {
     return "Internal Server Error: " + ex.getMessage();
   }
